6af9c543628edc5af636268fcca0aed5c9b96622
Ivan Misarosh <ivan.misarosh05@gmail.com>
2023-12-14 15:27:43 +0200

    оновлений код + доданий пошук за розширенням

--- TestServer/TestServer.cpp
+++ TestServer/TestServer.cpp
@@ -7,11 +7,92 @@
 #include <ctime>
 #include <winsock2.h>
 
+//include "Cache.h"
+
 #pragma comment(lib, "ws2_32.lib")
 
 namespace fs = std::filesystem;
 
 const int PORT = 12345;  // Choose a port
+
+std::vector<std::string> DiveInformation(char buffer[])
+{
+    std::string userInformation(buffer);
+    std::istringstream iss(userInformation);
+    std::vector<std::string> result;
+
+    std::string token;
+    while (std::getline(iss, token, '|')) {
+        // Позбавтеся від зайвих пробілів на початку і в кінці кожної частини
+        size_t start = token.find_first_not_of(" ");
+        size_t end = token.find_last_not_of(" ");
+        if (start != std::string::npos && end != std::string::npos) {
+            result.push_back(token.substr(start, end - start + 1));
+        }
+    }
+    return result;
+}
+
+DWORD WINAPI ProcessClient(LPVOID lpParameter)
+{
+    SOCKET clientSocket = (SOCKET)lpParameter;
+
+    while (true) {
+
+        char buffer[1024];
+        int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesRead > 0) {
+            buffer[bytesRead] = '\0';
+            auto userInformation = DiveInformation(buffer);
+
+            std::cout << "Received directory path from client: " << userInformation[0] << std::endl;
+            std::cout << "Received file extension from client: " << userInformation[1] << std::endl;
+
+            std::string directoryContents;
+
+            try {
+
+                for (const auto& entry : std::filesystem::directory_iterator(userInformation[0]))
+                {
+                    if (entry.path().extension() == userInformation[1])
+                    {
+                        auto time_point = std::chrono::time_point_cast<std::chrono::system_clock::duration>
+                            (entry.last_write_time() - fs::file_time_type::clock::now() + std::chrono::system_clock::now());
+                        std::time_t time_t_value = std::chrono::system_clock::to_time_t(time_point);
+
+                        std::tm localTime;
+                        localtime_s(&localTime, &time_t_value);
+
+                        std::ostringstream oss;
+                        oss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
+
+                        directoryContents += entry.path().filename().string() + "\t|\t"
+                            + std::to_string(fs::file_size(entry)) + " bytes\t|\t"
+                            + oss.str()
+                            + "\n";
+                    }
+                }
+
+                send(clientSocket, directoryContents.c_str(), directoryContents.size(), 0);
+                std::cout << "Directory contents sent to the client.\n";
+            }
+            //}
+            catch (const std::filesystem::filesystem_error& e) {
+                std::cerr << "Error accessing directory: " << e.what() << std::endl;
+                // Send an error message to the client if needed
+            }
+            catch (...)
+            {
+                std::cout << "Error: " << GetLastError() << std::endl;
+            }
+        }
+
+    }
+
+    closesocket(clientSocket);
+    return 0;
+
+}
 
 void server() {
     WSADATA wsaData;
@@ -47,8 +128,8 @@ void server() {
     }
 
     std::cout << "Server is listening for incoming connections...\n";
-
-    while (true) {
+    while (true)
+    {
         SOCKET clientSocket = accept(serverSocket, NULL, NULL);
         if (clientSocket == INVALID_SOCKET) {
             std::cerr << "Accept failed. Error code: " << WSAGetLastError() << std::endl;
@@ -56,51 +137,16 @@ void server() {
             WSACleanup();
             return;
         }
-
         std::cout << "Client connected.\n";
-
-        char buffer[1024];
-        int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesRead > 0) {
-            buffer[bytesRead] = '\0';
-            std::string clientDirectory(buffer);
-            std::cout << "Received directory path from client: " << clientDirectory << std::endl;
-
-            std::string directoryContents;
-            try {
-                for (const auto& entry : std::filesystem::directory_iterator(clientDirectory)) {
-                    auto time_point = std::chrono::time_point_cast<std::chrono::system_clock::duration>(entry.last_write_time() - fs::file_time_type::clock::now() + std::chrono::system_clock::now());
-                    std::time_t time_t_value = std::chrono::system_clock::to_time_t(time_point);
-
-                    std::tm localTime;
-                    localtime_s(&localTime, &time_t_value);
-
-                    std::ostringstream oss;
-                    oss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
-
-                    directoryContents += entry.path().filename().string() + " | "
-                        + std::to_string(fs::file_size(entry)) + " bytes | "
-                        + oss.str()
-                        + "\n";
-                }
-
-                send(clientSocket, directoryContents.c_str(), directoryContents.size(), 0);
-                std::cout << "Directory contents sent to the client.\n";
-            }
-            catch (const std::filesystem::filesystem_error& e) {
-                std::cerr << "Error accessing directory: " << e.what() << std::endl;
-                // Send an error message to the client if needed
-            }
-        }
-
-        closesocket(clientSocket);
+        CreateThread(NULL, NULL, ProcessClient, (LPVOID)clientSocket, NULL, NULL);
     }
 
     closesocket(serverSocket);
     WSACleanup();
 }
 
-int main() {
+int main()
+{
     server();
     return 0;
 }
